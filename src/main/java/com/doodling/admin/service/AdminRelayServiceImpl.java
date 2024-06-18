929f6dd02b5c91bf877efdbe29e210b41deb50ba
wlgus253254 <wlgus253254@ewhain.net>
2024-06-18 23:25:18 +0900

    :sparkles: Feat : 공모전 종료

--- src/main/java/com/doodling/admin/service/AdminRelayServiceImpl.java
+++ src/main/java/com/doodling/admin/service/AdminRelayServiceImpl.java
@@ -2,6 +2,8 @@ package com.doodling.admin.service;
 
 import com.doodling.admin.dto.RelayInsertRequestDTO;
 import com.doodling.admin.mapper.AdminRelayMapper;
+import com.doodling.exception.CustomException;
+import com.doodling.exception.ErrorCode;
 import com.doodling.relay.domain.Relay;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,20 +17,25 @@ public class AdminRelayServiceImpl implements AdminRelayService {
     private final AdminRelayMapper mapper;
 
     @Override
-    public void insertRelay(RelayInsertRequestDTO request) throws Exception {
-        try {
-            Relay relay = Relay.builder()
-                    .title(request.getTitle())
-                    .cover(request.getCover())
-                    .age(request.getAge())
-                    .startdate(request.getStartdate())
-                    .enddate(request.getEnddate())
-                    .build();
-            log.info("insert relay -> " + relay);
-            mapper.insertRelay(relay);
-        } catch (Exception e) {
-            log.warn(e.getMessage());
-            throw e;
-        }
+    public void insertRelay(RelayInsertRequestDTO request) {
+        Relay relay = Relay.builder()
+                .title(request.getTitle())
+                .cover(request.getCover())
+                .age(request.getAge())
+                .startdate(request.getStartdate())
+                .enddate(request.getEnddate())
+                .build();
+        log.info("insert relay -> " + relay);
+        mapper.insertRelay(relay);
+    }
+
+    @Override
+    public boolean deleteRelay(Integer relayId) {
+        log.info("delete relay -> " + relayId);
+        int result = mapper.deleteRelay(relayId);
+
+        if (result == 0) throw new CustomException(ErrorCode.FAIL_TO_DELETE);
+
+        return result == 1;
     }
 }
