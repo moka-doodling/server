3f5212023c10fb8df66553d6145bc1447ba753fe
김지수 <wltn2858@swu.ac.kr>
2024-06-19 11:46:58 +0900

    :hammer: Fix : token 유효기간 수정

--- src/main/java/com/doodling/member/dto/LoginRequestDTO.java
+++ src/main/java/com/doodling/member/dto/LoginRequestDTO.java
@@ -1,11 +1,13 @@
 package com.doodling.member.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Getter
 public class LoginRequestDTO {
 
